7634fb3d5fe9c59a7713718f703f811ad0fe9bfd
ELB <phantom.dev.elb@gmail.com>
2019-01-05 23:39:35 -0800

    Adds p1 folder and page

---
+++ itc250/p1/functions.php
@@ -0,0 +1,13 @@
+<?php
+
+//functions.php
+
+function sayHi()
+{
+	echo "Hello world.";
+}
+
+function fToC($far)
+{
+	return ($far-32)*(5/9);
+}
---
+++ itc250/p1/index.php
@@ -0,0 +1,15 @@
+<?php
+
+//index.php
+
+require 'functions.php';
+
+SayHi();	//Quick test of functions
+
+echo '<br/>';
+
+//Basically, the number_format takes the number in the first argument, then the number of decimals as the second.
+//Other arguments can be given, but they don't matter much and are optional.
+//https://www.w3schools.com/php/func_string_number_format.asp
+echo '79F is equal toooooo ';
+echo number_format(fToC(79),2);
