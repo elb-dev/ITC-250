60ab61e869fb719be09086f1ff15f7babb49072a
Derek Hendrick <phantom.dev.elb@gmail.com>
2019-01-10 17:45:53 -0800

    Makes radio buttons fully functional
    Also adds degree symbols on F and C

--- itc250/p1/index.php
+++ itc250/p1/index.php
@@ -4,26 +4,70 @@
 require 'functions.php';
 
 
-echo '<br/>';
-
-//Basically, the number_format takes the number in the first argument, then the number of decimals as the second.
-//Other arguments can be given, but they don't matter much and are optional.
-//https://www.w3schools.com/php/func_string_number_format.asp
-echo number_format(myFToC(79),2);
-echo '<br/>';
-
-echo number_format(myCToF(79),2);
-echo '<br/>';
-
-echo number_format(myFToK(79),2);
-echo '<br/>';
-
-echo number_format(myKToF(79),2);
-echo '<br/>';
-
-echo number_format(myCToK(79),2);
-echo '<br/>';
-
-echo number_format(myKToC(79),2);
-echo '<br/>';
-
+if(isset($_POST["InitTemp"]) && $_POST["StartTempScale"] != $_POST["EndTempScale"]){
+    //Conversions
+    $inputTemp = $_POST["InitTemp"];
+    $StartTempScale = $_POST["StartTempScale"];
+    $EndTempScale = $_POST["EndTempScale"];
+    
+    echo '<div>';
+    if($StartTempScale == 0){
+        if($EndTempScale == 1){
+            //F to C
+            $outputTemp = myFToC($inputTemp);
+            echo "$inputTemp &#8457; to $outputTemp &#8451;";
+            echo "<br />";
+        }else{
+            //F to K
+            $outputTemp = myFToK($inputTemp);
+            echo "$inputTemp &#8457; to $outputTemp &#8490;";
+            echo "<br />";
+        }
+    }elseif($StartTempScale == 1){
+        if($EndTempScale == 0){
+            //C to F
+            $outputTemp = myCToF($inputTemp);
+            echo "$inputTemp &#8451; to $outputTemp &#8457;";
+            echo "<br />";
+        }else{
+            //C to K
+            $outputTemp = myCToK($inputTemp);
+            echo "$inputTemp &#8451; to $outputTemp &#8490;";
+            echo "<br />";
+        }
+    }elseif($StartTempScale == 2){
+        if($EndTempScale == 1){
+            //K to C
+            $outputTemp = myKToC($inputTemp);
+            echo "$inputTemp &#8490; to $outputTemp &#8451;";
+            echo "<br />";
+        }else{
+            //K to F
+            $outputTemp = myKToF($inputTemp);
+            echo "$inputTemp &#8490; to $outputTemp &#8457;";
+            echo "<br />";
+        }
+    }
+    
+    echo '</div>';
+}else{
+    echo'
+    <div>
+    <form action="" method="post">
+    Start Temperature Scale: 
+    &#8457;<input type="radio" name="StartTempScale" value=0 required />
+    &#8451;<input type="radio" name="StartTempScale" value=1 required />
+    &#8490;<input type="radio" name="StartTempScale" value=2 required />
+    <br />
+    Temperature: <input type="number" name="InitTemp" required/>
+    <br />
+    End Temperature Scale: 
+    &#8457;<input type="radio" name="EndTempScale" value=0 required />
+    &#8451;<input type="radio" name="EndTempScale" value=1 required />
+    &#8490;<input type="radio" name="EndTempScale" value=2 required />
+    <br/ >
+    <input type="submit" />
+    </form>
+    </div>
+    ';
+}
--- itc250/sandbox/classes1.php
+++ itc250/sandbox/classes1.php
@@ -1,16 +1,33 @@
 <?php
 //classes1.php
 
-$myPerson = new Person();
+$myPeople[] = new Person("Parker","Peter",17);
+$myPeople[] = new Person("Stark","Tony",41);
+$myPeople[] = new Person("Odinson","Thor",274);
+$myPeople[] = new Person("Rominov","Natasha",31);
 
-echo $myPerson->Age;
+foreach($myPeople as $myPerson){
+    echo "<p>My name is $myPerson->FirstName $myPerson->LastName and I'm $myPerson->Age years old.</p>";
+}
+
+/*
+echo '<pre>';
+var_dump($myPerson);
+echo '</pre>';
+*/
 
 class Person
 {
-    //init
     //fields
-    public $FirstName = ''; //default value of empty string
     public $LastName = '';
+    public $FirstName = ''; //default value of empty string
     public $Age = 0; //default value of empty number is 0
+    //init
+    public function __construct($LastName,$FirstName,$Age)
+    {
+        $this->LastName = $LastName;
+        $this->FirstName = $FirstName;
+        $this->Age = $Age;
+    }
     //methods
 }
